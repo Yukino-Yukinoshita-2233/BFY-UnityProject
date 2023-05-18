8c0b18b230ec2b5c4304e20ab9a40b254590e97c
Yukino-Yukinoshita-2233 <zero2410465558@outlook.com>
2023-05-18 16:13:46 +0800

    更新主世界怪物,怪物UI,角色血条UI

--- Assets/Scripts/Moster/MonsterHpBar.cs
+++ Assets/Scripts/Moster/MonsterHpBar.cs
@@ -5,9 +5,7 @@ using UnityEngine.UI;
 
 public class MonsterHpBar : MonoBehaviour
 {
-    public Transform Monster;
-    public Transform MonsterHp;
-    private Transform mainCameraTransform;
+    Transform mainCameraTransform;
 
     Slider Slider1; 
     public float MonsterHP = 100;
@@ -18,25 +16,22 @@ public class MonsterHpBar : MonoBehaviour
         // 获取场景中的 Main Camera 对象的 Transform 组件
         mainCameraTransform = Camera.main.transform;
         Slider1 = gameObject.GetComponent<Slider>();
-        Slider1.value = MonsterHP / 100;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
         Follow();
-        Slider1.value = MonsterHP / 100;
-
+        if(Slider1!=null)
+        {
+            Slider1.value = MonsterHP * 0.01f;
+        }
+        
     }
     //跟随
     void Follow()
     {
         //MonsterHp.transform.position = Camera.main.WorldToScreenPoint(Monster.transform.position + Vector3.up);
-        MonsterHp.transform.position = Monster.transform.position+ Vector3.up * 3;
         transform.LookAt(transform.position + mainCameraTransform.rotation * Vector3.forward,mainCameraTransform.rotation * Vector3.up);
-
-
     }
 }
