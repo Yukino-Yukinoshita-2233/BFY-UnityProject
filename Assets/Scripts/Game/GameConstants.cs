3697264da37ca51eb9a402908d9f0122b55b0cfe
Arrivalm <2909405306@qq.com>
2023-05-23 21:12:28 +0800

    Player: third person

---
+++ Assets/Scripts/Game/GameConstants.cs
@@ -0,0 +1,10 @@
+namespace Unity.Game.Constants
+{
+  public class GameConstants
+  {
+    public const string AxisNameVertical = "Vertical";
+    public const string AxisNameHorizontal = "Horizontal";
+    public const string MouseAxisNameVertical = "Mouse Y";
+    public const string MouseAxisNameHorizontal = "Mouse X";
+  }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Game.Constants;
 
 public class PlayerController : MonoBehaviour
 {
+  public float rotationSpeed = 200f;
   private float moveSpeed = 1.5f;
+  private float mouseX;
+  private float mouseY;
   private Animator animator;
   private CharacterController controller;
   private int speedHash;
@@ -12,9 +16,17 @@ public class PlayerController : MonoBehaviour
 
   void Start()
   {
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+
     animator = GetComponent<Animator>();
     controller = GetComponent<CharacterController>();
 
+    GetAnimatorHash();
+  }
+
+  void GetAnimatorHash()
+  {
     speedHash = Animator.StringToHash("speed");
     isRunHash = Animator.StringToHash("isRun");
   }
@@ -24,8 +36,8 @@ public class PlayerController : MonoBehaviour
     var info = animator.GetCurrentAnimatorStateInfo(0);
     if (!info.IsTag("Attack"))
     {
-      float horizontal = Input.GetAxis("Horizontal");
-      float vertical = Input.GetAxis("Vertical");
+      float horizontal = Input.GetAxis(GameConstants.AxisNameHorizontal);
+      float vertical = Input.GetAxis(GameConstants.AxisNameVertical);
       Vector3 dir = new Vector3(horizontal, 0, vertical);
       if (dir != Vector3.zero)
       {
@@ -38,5 +50,18 @@ public class PlayerController : MonoBehaviour
         animator.SetBool(isRunHash, false);
       }
     }
+    MouseController();
   }
+
+  void MouseController()
+  {
+    mouseX = Input.GetAxis(GameConstants.MouseAxisNameHorizontal) * rotationSpeed * Time.deltaTime;
+    transform.Rotate(new Vector3(0f, mouseX, 0f), Space.Self);
+
+    mouseY -= Input.GetAxis(GameConstants.MouseAxisNameVertical) * rotationSpeed * Time.deltaTime;
+    mouseY = Mathf.Clamp(mouseY, -50f, 50f);
+    Camera.main.transform.localEulerAngles = new Vector3(mouseY, 0, 0);
+
+  }
+
 }
