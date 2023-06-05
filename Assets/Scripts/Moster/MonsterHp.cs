dffdc725d74b015d7852a2f0fcae6f4b884acd26
Yukino-Yukinoshita-2233 <zero2410465558@outlook.com>
2023-06-05 11:20:22 +0800

    修复血条BUG

--- Assets/Scripts/Moster/MonsterHp.cs
+++ Assets/Scripts/Moster/MonsterHp.cs
@@ -5,21 +5,22 @@ using UnityEngine.UI;
 
 public class MonsterHp : MonoBehaviour
 {
-  [SerializeField]
-  Slider Slider1;
-  public float MonsterHP { get; set; } = 100;
+    [SerializeField]
+    Slider Slider1;
 
-  void Start()
-  {
-    Slider1 = gameObject.gameObject.GetComponent<Slider>();
-  }
+    public float MonsterHP { get; set; } = 100;
 
-  // Update is called once per frame
-  void Update()
-  {
-    if (Slider1 != null)
+    void Start()
     {
-      Slider1.value = MonsterHP * 0.01f;
+        Slider1 = gameObject.GetComponentInChildren<Slider>();
     }
-  }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Slider1 != null)
+        {
+            Slider1.value = MonsterHP * 0.01f;
+        }
+    }
 }
